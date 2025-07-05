d33dd09e07ff5e90c66ef34646dd92082399ff3d
Lokno <lokno1@gmail.com>
2025-07-04 21:48:37 -0400

    Removed unused includes

--- src/PEAKSleepTalk/ConfigurationManager.cs
+++ src/PEAKSleepTalk/ConfigurationManager.cs
@@ -1,8 +1,6 @@
 ﻿using BepInEx.Configuration;
 using BepInEx;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace PEAKSleepTalk
 {
--- src/PEAKSleepTalk/EnableSleepTalkPatch.cs
+++ src/PEAKSleepTalk/EnableSleepTalkPatch.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 
-
 namespace PEAKSleepTalk
 {
     internal class EnableSleepTalkPatch
